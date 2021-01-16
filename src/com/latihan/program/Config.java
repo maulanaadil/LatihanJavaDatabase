dae57cfc9b2edd14b32b4f6784a24228bc3af9a9
MaulanaAdil <kenari222@gmail.com>
2021-01-16 23:20:08 +0700

    First Commit

---
+++ src/com/latihan/program/Config.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.latihan.program;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Config {
+    
+    private static Connection MySQLConfig;
+    
+    public static Connection configDB() throws SQLException {
+        try {
+            String url = "jdbc:mysql://localhost:3306/latihanjavadatabase";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+            System.out.println("Success Connect to Database");
+        } catch (SQLException e) {
+            System.out.println("Failed Connect to Database" + e.getMessage());
+        }
+        return MySQLConfig;
+    } 
+}
